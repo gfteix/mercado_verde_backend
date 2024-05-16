23421755fc9d125577031d30ecb5fcff8be91655
gfteix <gabriel.fteixeira13@gmail.com>
2024-05-16 20:19:55 -0300

    feat: add more seed data

--- src/database/seed.ts
+++ src/database/seed.ts
@@ -11,17 +11,17 @@ async function seed() {
 
     const frutaCategory = new Category();
     frutaCategory.name = "Frutas";
-    frutaCategory.imageUrl = "frutas.png";
+    frutaCategory.imageUrl = "frutas.jpg";
     categories.push(frutaCategory);
 
     const verduraCategory = new Category();
     verduraCategory.name = "Verduras";
-    verduraCategory.imageUrl = "verduras.png";
+    verduraCategory.imageUrl = "verduras.jpg";
     categories.push(verduraCategory);
 
     const legumesCategory = new Category();
     legumesCategory.name = "Legumes";
-    legumesCategory.imageUrl = "legumes.png";
+    legumesCategory.imageUrl = "legumes.jpg";
     categories.push(legumesCategory);
 
     const products: Product[] = [];
@@ -29,7 +29,7 @@ async function seed() {
     const laranja = new Product();
     laranja.name = "Laranja";
     laranja.category = frutaCategory;
-    laranja.imageUrl = "laranja.png";
+    laranja.imageUrl = "laranja.jpg";
     laranja.price = 100;
 
     products.push(laranja);
@@ -37,10 +37,42 @@ async function seed() {
     const kiwi = new Product();
     kiwi.name = "Kiwi";
     kiwi.category = frutaCategory;
-    kiwi.imageUrl = "kiwi.png";
-    kiwi.price = 100;
+    kiwi.imageUrl = "kiwi.jpg";
+    kiwi.price = 200;
 
     products.push(kiwi);
+
+    const limaoSiciliano = new Product();
+    limaoSiciliano.name = "Limão Siciliano";
+    limaoSiciliano.category = frutaCategory;
+    limaoSiciliano.imageUrl = "limao_siciliano.jpg";
+    limaoSiciliano.price = 430;
+
+    products.push(limaoSiciliano);
+
+    const pimentao = new Product();
+    pimentao.name = "Pimentão";
+    pimentao.category = legumesCategory;
+    pimentao.imageUrl = "pimentao.jpg";
+    pimentao.price = 550;
+
+    products.push(pimentao);
+
+    const alface = new Product();
+    alface.name = "Alface";
+    alface.category = verduraCategory;
+    alface.imageUrl = "alface.jpg";
+    alface.price = 200;
+
+    products.push(alface);
+
+    const cebola = new Product();
+    cebola.name = "Cebola";
+    cebola.category = legumesCategory;
+    cebola.imageUrl = "cebola.jpg";
+    cebola.price = 330;
+
+    products.push(cebola);
 
     await AppDataSource.getRepository(Product).save(products);
   }
