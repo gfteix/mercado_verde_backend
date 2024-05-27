4f978f8eddfaa9e462c6f67a40aa499bb0ede616
gfteix <gabriel.fteixeira13@gmail.com>
2024-05-27 11:04:36 -0300

    fix: cors

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,16 @@ const app = express();
 
 app.use(express.json());
 app.use(logger);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+  );
+  next();
+});
+
 app.use("/api/v1", routes);
 app.use(errorHandler);
 
